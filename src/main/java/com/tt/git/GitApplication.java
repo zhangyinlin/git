2bb4a5e3aa941ab536a743e6d684aa6e8dc7d8f7
zhangyinlin <1023213308@qq.com>
2018-12-15 17:17:03 +0800

    创建git

---
+++ src/main/java/com/tt/git/GitApplication.java
@@ -0,0 +1,14 @@
+package com.tt.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tt/git/GitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tt.git;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
